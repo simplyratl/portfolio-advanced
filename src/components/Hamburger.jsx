ca4f4ecc9e0eda207a8de56eff47128dce377748
nikicaraznatovic <nikicaraznatovic123@gmail.com>
2023-03-20 22:34:32 +0100

    popravke hamburger menija

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -5,20 +5,21 @@ import { useMenuStore } from "../utils/store";
 import { LightModeContext } from "../context/lightModeContext/LightModeContext";
 
 const HamburgerMenu = () => {
-	const { lightMode } = useContext(LightModeContext);
-	const { menuOpen, menuToggle } = useMenuStore();
+  const { lightMode } = useContext(LightModeContext);
+  const { menuOpen, menuToggle } = useMenuStore();
 
-	return (
-		<div className="fixed top-4 left-8 w-fit z-50" onClick={menuToggle}>
-			<Hamburger
-				rounded
-				label="Menu"
-				size={48}
-				color={lightMode ? "#a8a6a5" : "#57595a"}
-			/>
-			<p className="text-white text-4xl">{menuOpen}</p>
-		</div>
-	);
+  return (
+    <div className="fixed top-4 left-8 w-fit z-50" onClick={menuToggle}>
+      <Hamburger
+        rounded
+        label="Menu"
+        size={48}
+        toggled={menuOpen}
+        color={lightMode ? "#a8a6a5" : "#57595a"}
+      />
+      <p className="text-white text-4xl">{menuOpen}</p>
+    </div>
+  );
 };
 
 export default HamburgerMenu;
--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -5,39 +5,41 @@ import { motion } from "framer-motion";
 import { Link } from "react-scroll";
 
 const HeaderMenu = () => {
-	const nav = ["Home", "About", "Projects", "Contant"];
+  const { menuToggle } = useMenuStore();
+  const nav = ["Home", "About", "Projects", "Contant"];
 
-	return (
-		<motion.div
-			initial={{ opacity: 0, x: 60 }}
-			animate={{ opacity: 1, x: 0 }}
-			exit={{ opacity: 0, x: 60 }}
-			transition={{ duration: 0.4, exit: { duration: 1 }, ease: "easeInOut" }}
-			className="fixed top-0 left-0 w-[200px] h-screen z-[1]"
-		>
-			<ul className="h-full pt-20 flex flex-col gap-2 pl-8">
-				{nav.map((item, index) => (
-					<li
-						key={index}
-						className="text-white text-2xl font-bold hover:text-[#d33333]"
-					>
-						<Link
-							to={item.toLowerCase()}
-							smooth={true}
-							offset={-40}
-							duration={500}
-							delay={80}
-							spy={true}
-							isDynamic
-							activeStyle={{ color: "#d33333" }}
-						>
-							{item}
-						</Link>
-					</li>
-				))}
-			</ul>
-		</motion.div>
-	);
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: 60 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 60 }}
+      transition={{ duration: 0.4, exit: { duration: 1 }, ease: "easeInOut" }}
+      className="fixed top-0 left-0 h-screen z-[1] w-full"
+      onClick={menuToggle}
+    >
+      <ul className="w-fit h-full pt-20 flex flex-col gap-2 pl-8">
+        {nav.map((item, index) => (
+          <li
+            key={index}
+            className="text-white text-2xl font-bold hover:text-[#d33333]"
+          >
+            <Link
+              to={item.toLowerCase()}
+              smooth={true}
+              offset={-40}
+              duration={500}
+              delay={80}
+              spy={true}
+              isDynamic
+              activeStyle={{ color: "#d33333" }}
+            >
+              {item}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </motion.div>
+  );
 };
 
 export default HeaderMenu;
